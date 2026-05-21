6895590627cad9c57645a63f1de9a94fe6a52641
osor6037-prog <osor6037@gmail.com>
2026-05-21 22:09:35 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,31 @@
+const firebaseConfig = {
+
+  apiKey:
+  "AIzaSyBLK_mFbKr5OaHMUxz-Ec9F5IFIAWl0U1M",
+
+  authDomain:
+  "doujin-pop.firebaseapp.com",
+
+  databaseURL:
+  "https://doujin-pop-default-rtdb.asia-southeast1.firebasedatabase.app",
+
+  projectId:
+  "doujin-pop",
+
+  storageBucket:
+  "doujin-pop.firebasestorage.app",
+
+  messagingSenderId:
+  "392625657306",
+
+  appId:
+  "1:392625657306:web:346f6cd2d1be1f1eaedce3",
+
+  measurementId:
+  "G-6JE42TPDTS"
+
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database();
